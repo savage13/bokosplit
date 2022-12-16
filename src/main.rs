1fd196d7b07f941579b3313465bed2a1c8ec13a9
Brian Savage <savage13@gmail.com>
2022-12-15 19:10:10 -0500

    Open, Save, Hide, and comparisons switch

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@
 use core::time::Duration;
 use eframe::egui;
 use livesplit_core::rendering::software;
+use livesplit_core::run::parser::composite;
 use livesplit_core::run::saver::livesplit;
 use livesplit_core::{Layout, Run, Segment, SharedTimer, Timer};
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::BufWriter;
+use std::io::{BufReader, BufWriter};
+use std::path::PathBuf;
 //use livesplit_core::{HotkeyConfig, HotkeySystem, Layout, Run, Segment, SharedTimer, Timer};
 //use livesplit_hotkey;
 
@@ -23,6 +25,8 @@ fn main() {
     );
 }
 
+const SPLIT_SAVE: &'static str = "boko_timer_splits.lss";
+
 type Callback = fn(&mut MyApp);
 struct MyApp {
     stimer: SharedTimer,
@@ -33,15 +37,29 @@ struct MyApp {
     frame: egui::Frame,
 }
 
+fn read_file(path: PathBuf) -> Option<Run> {
+    let file = File::open(&path).ok()?;
+    let file = BufReader::new(file);
+    let load_files = true;
+    // Actually parse the file.
+    let result = composite::parse(file, Some(path), load_files);
+    let parsed = result.expect("Not a valid splits file");
+    Some(parsed.run)
+}
+
 impl MyApp {
     fn split(app: &mut MyApp) {
+        dbg!("split");
         app.stimer.write().split_or_start();
         app.save_state();
     }
     fn save_state(&mut self) {
+        self.save_file(&PathBuf::from(SPLIT_SAVE));
+    }
+    fn save_file(&mut self, filename: &PathBuf) {
         let timer = self.stimer.read();
         let snapshot = timer.snapshot();
-        let file = File::create("splits_file.lss");
+        let file = File::create(filename);
         let writer = BufWriter::new(file.expect("Failed creating the file"));
         livesplit::save_timer(&snapshot, writer).expect("Couldn't save the splits file");
     }
@@ -59,6 +77,43 @@ impl MyApp {
     fn pause(app: &mut MyApp) {
         app.stimer.write().toggle_pause();
         app.save_state();
+    }
+    fn save(app: &mut MyApp) {
+        dbg!("save");
+        app.stimer.write().reset(true);
+        let mut path = app.stimer.read().run().path().clone();
+        if path.is_none() {
+            path = rfd::FileDialog::new()
+                .add_filter("Livesplit", &["lss"])
+                .save_file()
+        }
+        if let Some(filename) = path {
+            app.save_file(&filename);
+        }
+    }
+    fn open(app: &mut MyApp) {
+        println!("open");
+        if let Some(path) = rfd::FileDialog::new().pick_file() {
+            dbg!(&path);
+            if let Some(run) = read_file(path) {
+                app.stimer
+                    .write()
+                    .replace_run(run, false)
+                    .expect("Run has no segments, sus");
+            }
+        } else {
+            dbg!("no file picked");
+        }
+    }
+    fn comparison(app: &mut MyApp) {
+        app.stimer.write().switch_to_next_comparison();
+        dbg!(app.stimer.read().current_comparison());
+    }
+    fn hide(app: &mut MyApp) {
+        app.stimer
+            .write()
+            .set_current_comparison("None")
+            .expect("Could not set to None");
     }
     fn hotkey(&mut self, ctx: &egui::Context) {
         let keys: Vec<_> = self
@@ -92,14 +147,40 @@ impl MyApp {
 impl Default for MyApp {
     fn default() -> Self {
         // Create a run object that we can use with at least one segment.
-        let mut run = Run::new();
-        run.set_game_name("Breath of the Wild");
-        run.set_category_name("100%");
-        run.push_segment(Segment::new("Paraglider"));
-        run.push_segment(Segment::new("IST"));
-        run.push_segment(Segment::new("Vah Medoh"));
-        run.push_segment(Segment::new("Ganon"));
-        run.push_segment(Segment::new("Korok 900"));
+
+        let run = if let Some(run) = read_file(PathBuf::from(SPLIT_SAVE)) {
+            dbg!("previous run");
+            println!("game_name {}", run.game_name());
+            println!("category  {}", run.category_name());
+            println!("modified  {}", run.has_been_modified());
+            println!("last_attampt:");
+            let attempts = run.attempt_history();
+            let n = attempts.len();
+            if n > 0 {
+                let last = &attempts[n - 1];
+                let index = last.index();
+                println!("started: {:?}", last.started());
+                println!("ended: {:?}", last.ended());
+                let time = last.time();
+                if time.real_time.is_none() && time.game_time.is_none() {
+                    println!("attempt not finished");
+                } else {
+                    println!("attempt finished, possibly unsaved");
+                }
+            }
+            run
+        } else {
+            dbg!("new run");
+            let mut run = Run::new();
+            run.set_game_name("Breath of the Wild");
+            run.set_category_name("100%");
+            run.push_segment(Segment::new("Paraglider"));
+            run.push_segment(Segment::new("IST"));
+            run.push_segment(Segment::new("Vah Medoh"));
+            run.push_segment(Segment::new("Ganon"));
+            run.push_segment(Segment::new("Korok 900"));
+            run
+        };
 
         let stimer = Timer::new(run)
             .expect("Run with at least one segment provided")
@@ -117,6 +198,10 @@ impl Default for MyApp {
         func.insert(egui::Key::U, MyApp::undo_split);
         func.insert(egui::Key::S, MyApp::skip_split);
         func.insert(egui::Key::R, MyApp::reset);
+        func.insert(egui::Key::O, MyApp::open);
+        func.insert(egui::Key::S, MyApp::save);
+        func.insert(egui::Key::C, MyApp::comparison);
+        func.insert(egui::Key::H, MyApp::hide);
 
         let keys: Vec<_> = func.keys().cloned().collect();
 
